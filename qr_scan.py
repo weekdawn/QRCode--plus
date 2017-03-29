a00bbee8bb86b5d5600bc4ee95ea89317b3130e2
weekdawn <571426779@qq.com>
2017-03-29 08:55:18 +0800

    qr_scan.py:将扫描后的数据保存到当前的output文件夹下的ini配置文件中

--- qr_scan.py
+++ qr_scan.py
@@ -2,6 +2,8 @@
 import zbar
 from PIL import Image
 import sys
+import os
+import re
 
 reload(sys)
 #设置系统默认编码方式，避免中文乱码
@@ -27,6 +29,23 @@ def qrScan(file_name):
 			
 
 if __name__ == '__main__':
-	data_file = open("qr_data.ini", "a")
+	#判断ini文件是否有[Config]
+	match_config = re.compile("[Config]")
+	#判断是否存在output文件夹，若不存在则创建
+	if not os.path.exists("output"):
+		os.mkdir("output")
+	data_file = open("./output/qr_data.ini", "a+")
+	#定位到文件首位
+	data_file.seek(0,0)
+	first_line = data_file.readline()
+	#如果文首没有匹配到[Config]自动添加进去
+	if not re.search(match_config, first_line):
+		data_file.seek(0,0)
+		data_file.write("[Config]\n")
+	#定位到文末
+	data_file.seek(0,2)
+	#从命令行接收文件路径
 	file_name = sys.argv[1]
 	q = qrScan(file_name)
+	print "Scan QrCode successed!"
+	data_file.close()
